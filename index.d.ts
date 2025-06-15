ab96cca7796e87ceb33a5d0c4899033523dd42fd
ubyjerome <ubongabasijerome8@gmail.com>
2025-06-16 00:07:59 +0100

    Fix (Project): Invalid Type to Index Reference

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,17 @@
 declare module "jsonbin-orm" {
-    interface BinConfig {
-      binId: string;
-      apiKey: string;
-      schema: Record<string, string>;
-    }
-  
-    interface RecordData {
-      [key: string]: any;
-    }
-  
-    class Bin {
-      constructor(config: BinConfig);
-      createNew(data: RecordData): Promise<RecordData>;
-      fetchById(id: string): Promise<RecordData>;
-      updateOneById(id: string, data: RecordData): Promise<RecordData>;
-      deleteById(id: string): Promise<boolean>;
-    }
-  
-    export { Bin, BinConfig, RecordData };}
+  interface BinConfig {
+    binId: string;
+    apiKey: string;
+    schema: Object;
+  }
+
+  class bin {
+    constructor(config: BinConfig);
+    createNew(data: any): Promise<any>;
+    fetchById(id: string): Promise<any>;
+    updateOneById(id: string, data: any): Promise<any>;
+    deleteById(id: string): Promise<boolean>;
+  }
+
+  export { bin, BinConfig };
+}
